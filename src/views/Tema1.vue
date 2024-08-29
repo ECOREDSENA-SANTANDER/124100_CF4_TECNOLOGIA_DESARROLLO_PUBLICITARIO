41ce11a0335df3c46404d01ed1ae362b701fc216
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-08-29 10:31:29 -0500

    avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,9 +7,265 @@
           span 1
         h1 ¿Qué es una campaña publicitaria?
 
-     
-        
+      .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema1/img01.png", alt="")
+        .col-lg-8 
+          p Una campaña publicitaria es un plan de comunicación diseñado a corto o mediano plazo. Debido al crecimiento del mercado, la llegada constante de competidores, la globalización y la rapidez de la información, las campañas se planifican para periodos más cortos o con mayor frecuencia. Actualmente, es raro ver campañas de más de tres meses en los medios. Las redes sociales permiten realizarlas con menos inversión y producciones más sencillas y personalizadas. A pesar de ello, cualquier campaña sigue siendo un proceso de comunicación entre un anunciante y un consumidor, involucrando también a la agencia de publicidad, freelancers o grupos creativos.
+          .cajon.color-secundario.p-4.mb-4
+            .h5 El desarrollo de una campaña publicitaria abarca tres elementos clave: la investigación, el brief y la identidad corporativa. Estos componentes son fundamentales para el proceso de comunicación y establecen la ruta para una conversación eficaz con el cliente. Aunque cada agencia publicitaria tiene su propio modelo, todos siguen un proceso similar con elementos estándar que se describirán a continuación. Algunos de estos elementos se abordarán en mayor profundidad en otros materiales, por lo que aquí se presentan de manera básica.
+      
+      Separador
+      #t_1_1.titulo-segundo.color-acento-contenido
+        h2 1.1 Estructura de la campaña publicitaria
+      
+      p La estructura que se relaciona a continuación es estándar y contiene los elementos más relevantes, esto implica que existen métodos con otros puntos adicionales o que no se tienen en cuenta, o que se nombran de forma diferente:  
     
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico1.svg")
+              h3.bg13.py-2.w-card-text El brief
+      
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6
+          p Aquí, nos enfocaremos en el brief de campaña táctica, que destaca componentes cruciales para desarrollar el mensaje. Si la campaña es para una empresa con la que la agencia, grupo creativo o freelancer no ha tenido contacto previo, es recomendable también realizar un brief de primer contacto.
+          p Un brief debe responder a preguntas básicas: ¿por qué se hace la campaña?, ¿qué se quiere transmitir en el mensaje publicitario?, y ¿qué se espera que haga la agencia, grupo creativo o freelancer?.
+          p Los elementos más importantes del brief incluyen el análisis del consumidor, la descripción detallada del producto y el análisis de la competencia.
+        .col-lg-4 
+          img(src="@/assets/curso/temas/tema1/img02.png", alt="")
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico2.svg")
+              h3.bg13.py-2.w-card-text Descripción del producto o servicio
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema1/img03.png", alt="")
+        .col-lg-6 
+          p Para la agencia, el grupo creativo o el freelancer, uno de los elementos que mejor proveen datos para el desarrollo de la campaña, es el producto mismo y resulta muy importante que la empresa no deje escapar ningún detalle en relación con este; en ocasiones, es necesario que quien va a realizar la parte creativa de la campaña firme un acta de confidencialidad, con el fin de que el empresario otorgue toda la información posible sobre procesos de fabricación, canales de distribución, área de influencia, datos de la industria, participación en el mercado, protocolo de prestación del servicio, materias primas, proveedores, etc.
+          p Toda esta información representa posibles textos de apoyo, datos relevantes para la decisión de compra o incluso la base del concepto final.
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico3.svg")
+              h3.bg13.py-2.w-card-text Línea de productos
+      
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6
+          p Los productos de la empresa, por lo general tienen varias presentaciones, ya sea que se diferencien por su tamaño, sabor, aroma, olor, forma, tipo de empaque, entre otros aspectos. También se puede dar el caso de que la marca tenga una extensión; es decir, que bajo el mismo nombre se hayan creado productos diferentes, por ejemplo: el caso de la marca de una galleta que la utilizan para crear un helado y aprovechar su posicionamiento en el mercado, con el fin de generar ventas inmediatas.
+          p Es indispensable que el empresario entregue toda esta información a los creativos que van a diseñar la campaña, pues de allí pueden surgir datos importantes para la comunicación, textos de apoyo, acciones de marketing, activaciones de marca o cualquier otro recurso útil para persuadir al mercado objetivo. 
+        .col-lg-4 
+          img(src="@/assets/curso/temas/tema1/img04.png", alt="")
+
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico4.svg")
+              h3.bg13.py-2.w-card-text Mercado objetivo
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema1/img05.png", alt="")
+        .col-lg-6 
+          p Como vimos en materiales anteriores, tal vez el aspecto más relevante del proceso de la comunicación sea el conocimiento que se tenga del consumidor. En este punto los creativos deben ser muy enfáticos con el empresario, ya que es muy común que para el dueño de una mipyme, la cobertura del mercado sea muy amplia y no haya una información clara del análisis del consumidor; por eso es muy importante que se apliquen en detalle las variables demográficas y se realice su debida cuantificación; adicional y más importante aún, individualizar el mercado a través de las características psicográficas, pues con las cuales se crean las estrategias más efectivas de comunicación. 
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico5.svg")
+              h3.bg13.py-2.w-card-text Elementos de identidad corporativa
+      
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6
+          p Para la realización de una estrategia de comunicación que involucra el desarrollo de piezas y el uso de diferentes medios, es vital que la marca sea presentada y expuesta de acuerdo con los lineamientos del manual de identidad; teniendo en cuenta dentro de este documento: los criterios relacionados con el color, el uso de tipografías, el manejo del logo y su ubicación en las piezas gráficas, las áreas de reserva, los tamaños mínimos y variaciones permitidas para la marca, entre otros.
+          p No es definirlos o alterarlos para la campaña y favorecer el posicionamiento, toda vez que de esta forma se estandariza el manejo de la marca, creando en el consumidor una imagen definida con elementos que asocia a esta.
+        .col-lg-4 
+          img(src="@/assets/curso/temas/tema1/img06.png", alt="")
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico6.svg")
+              h3.bg13.py-2.w-card-text Personalidad de marca
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema1/img07.png", alt="")
+        .col-lg-6 
+          p Como ya se indicó, el desarrollo de una estrategia de comunicación debe ir de la mano con la identidad corporativa de la empresa. La identidad está compuesta de múltiples elementos, entre ellos la personalidad de marca, la cual contiene aspectos del mercado que generan empatía entre el producto y los consumidores; con base en esta información se puede definir el look de la comunicación, el tipo de lenguaje a utilizar, el tipo de imágenes o ilustraciones que deberán contener las piezas gráficas o audiovisuales que incluirá la campaña, debido a que la personalidad de marca está ligada a los aspectos cualitativos del mercado; es decir, a sus características psicográficas.
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema1/ico6.svg")
+              h3.bg13.py-2.w-card-text Objetivos de comunicación (enfocados en el mensaje)
+
+      .row.justify-content-center.mb-4
+        .col-lg-11
+          .bloque-texto-g.fondo-b.p-3.p-sm-4.p-md-5
+            .bloque-texto-g__img(
+              :style="{'background-image': `url(${require('@/assets/curso/temas/tema1/img08.png')})`}"
+            )
+            .bloque-texto-g__texto.p-5.mt-4.mb-4
+              h5.mb-0 De acuerdo con Vilajoana, Jiménez, González y Vila (2014):<br>
+              br
+              p.mb-0 “Un objetivo de comunicación se define como aquel que una organización se propone conseguir a través de sus procesos comunicativos o de sus comunicaciones; es decir, el resultado esperado de una decisión de marketing que un anunciante busca a través de la campaña publicitaria”.
+
+      p Los objetivos de comunicación se pueden direccionar a varios enfoques, de acuerdo con las necesidades de comunicación de la empresa. Por tal motivo, basados en los autores mencionados, se indican los siguientes:
+  
+      AcordionA(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")
+        .row(titulo="Objetivos de conocimiento")
+          .col-md-8.mb-4.mb-md-0
+            p Su intención principal es la de dar a conocer un producto, servicio o empresa; son aquellos mediante los que se busca crear o modificar el conocimiento que tienen los públicos sobre la compañía. Dichos objetivos pueden tener los siguientes enfoques: generar un recuerdo (notoriedad), comunicar las características del producto, servicio o empresa, informar de sus ventajas o beneficios, difundir su uso o aplicaciones, informar sobre aspectos de la compra, informar sobre promociones de ventas, actividades de relaciones públicas, etc.
+          .col-md-4.mb-4.mb-md-0
+            img(src="@/assets/curso/temas/tema1/img09.png")
+        
+        .row(titulo="Objetivos enfocados en la actitud")
+          .col-md-8.mb-4.mb-md-0
+            p Su intención es la de cambiar la forma cómo se percibe un producto; son los que persiguen generar cambios de los sentimientos, emociones y preferencias de las personas en referencia a una compañía o producto. Se pueden direccionar hacia reforzar o desarrollar motivaciones, eliminar o reducir frenos, construir, reforzar o modificar la imagen de un producto, servicio o empresa, y generar preferencia.
+          .col-md-4.mb-4.mb-md-0
+            img(src="@/assets/curso/temas/tema1/img10.png")
+        
+        .row(titulo="Objetivos enfocados en el comportamiento")
+          .col-md-8.mb-4.mb-md-0
+            p Su intención es producir en el consumidor una reacción conductual; son los que buscan que el público objetivo realice algún tipo de acción en relación con la empresa, producto o servicio; también conocido como “call to action”. Estos objetivos se pueden enfocar en: inducir a la prueba o compra del producto, servicio o marca, modificar hábitos o costumbres de los consumidores, crear un comportamiento de compra o consumo, fomentar la asistencia al punto de venta, incentivar que se hable bien del producto.
+            p Un objetivo de comunicación debe responder a las siguientes características básicas:
+            p Deben ser claros y concretos. Estar redactados de forma concisa y plantear clara y directamente lo que el anunciante quiere obtener, en qué medida y en qué tiempo.
+            p Los objetivos deben ser asumibles. Ya sea a nivel de presupuesto, es decir, que la empresa pueda absorber el costo económico que representará cumplir el objetivo, como también a nivel de realismo; lo que implica que se pueda llegar a lograr con un esfuerzo acorde al resultado esperado. No es conveniente que los objetivos adquieran un carácter utópico, ni tampoco que sean excesivamente fáciles de lograr, puesto que en ambos casos se puede producir una desmotivación de las personas involucradas en su consecución.
+            p Los objetivos deben ser flexibles. Es decir, se tienen que poder modificar o corregir en la medida de lo posible ante situaciones excepcionales o particulares que se le presenten a la organización, sean estas de carácter positivo o negativo. (Vilajoana, et al., 2014).
+          .col-md-4.mb-4.mb-md-0
+            img(src="@/assets/curso/temas/tema1/img11.png")
+        
+        .row(titulo="Objetivos de medios (cuantitativos)")
+          .col-md-8.mb-4.mb-md-0
+            p Su intención principal es la de dar a conocer un producto, servicio o empresa; son aquellos mediante los que se busca crear o modificar el conocimiento que tienen los públicos sobre la compañía. Dichos objetivos pueden tener los siguientes enfoques: generar un recuerdo (notoriedad), comunicar las características del producto, servicio o empresa, informar de sus ventajas o beneficios, difundir su uso o aplicaciones, informar sobre aspectos de la compra, informar sobre promociones de ventas, actividades de relaciones públicas, etc.
+          .col-md-4.mb-4.mb-md-0
+            img(src="@/assets/curso/temas/tema1/img09.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
